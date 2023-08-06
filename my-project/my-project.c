b1661f7f0b08486edada0a8672b0db26a1c50b0a
Alpatiev Nikita <chococacaococonut@gmail.com>
2023-08-06 22:41:31 +0300

    done blink test

--- my-project/my-project.c
+++ my-project/my-project.c
@@ -1,9 +1,27 @@
-#include "api.h"
-#include "api-asm.h"
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
 
 int main(void) {
-	/* add your own code */
-	uint32_t rev = 0xaabbccdd;
-	rev = rev_bytes(rev);
-	return my_func(rev);
+	rcc_periph_clock_enable(RCC_GPIOC);
+
+	gpio_set_mode (
+		GPIOC,
+		GPIO_MODE_OUTPUT_2_MHZ,
+		GPIO_CNF_OUTPUT_PUSHPULL,
+		GPIO13
+	);
+
+	while (1) {
+		// 1 * 10^7 - const
+		// 1 * 10^6 - around second
+		// 1 * 10^5 - 100ms
+
+		for (int i = 0; i < 100000; i++) {
+			__asm__("nop");
+		}
+
+		gpio_toggle(GPIOC, GPIO13);
+	}
+
+	return 0;
 }
